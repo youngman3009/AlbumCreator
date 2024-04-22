9749a27fe06ad867d2db3c407f5d8b9df4e18b0a
Owen Youngman <youngman.owen@gmail.com>
2024-04-22 01:54:35 +0100

    feat: start db connection

--- albumcreator-backend-node/src/index.ts
+++ albumcreator-backend-node/src/index.ts
@@ -1,7 +1,16 @@
+import "reflect-metadata";
 import { app } from './app';
+import { AppDataSource } from "./data-source";
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Initialise database connection
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connection successful")
+    
+    // Start the server
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error("Error: ", error))
