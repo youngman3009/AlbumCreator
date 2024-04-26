c24c393d2e83b48ab3aeee0e5c1da428202f4917
Owen Youngman <youngman.owen@gmail.com>
2024-04-26 02:50:38 +0100

    feat: add endpoints for modifying albums

--- albumcreator-backend-node/src/controllers/album.ts
+++ albumcreator-backend-node/src/controllers/album.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { Album } from '../models/album';
+import { AppDataSource } from '../data-source';
+import { Track } from '../models/track';
 
 export const getAlbums = async (req: Request, res: Response) => {
   try {
@@ -18,7 +20,86 @@ export const getAlbum = async (req: Request<{ albumId: number }, {}, {}>, res: R
     if (!album) return res.status(400).json({ error: 'Album ID does not exist' });
     return res.json(album);
   } catch (error) {
+    console.error('Error getting album:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const createAlbum = async (req: Request<{}, {}, { name: string }>, res: Response) => {
+  try {
+    const { name } = req.body;
+    if (!name) return res.status(400).json({ error: 'Name is required' });
+    if (await albumExistsByName(name)) return res.status(400).json({ error: 'Name must be unique' });
+    const newAlbum = AppDataSource.getRepository(Album).create({
+      name
+    });
+    await newAlbum.save();
+    return res.status(201).json(newAlbum);
+  } catch (error) {
     console.error('Error getting albums:', error);
     return res.status(500).json({ error: 'Internal server error' });
   }
+};
+
+export const addTrackToAlbum = async (req: Request<{ albumId: number; trackId: number }, {}, {}>, res: Response) => {
+  try {
+    const { albumId, trackId } = req.params;
+    const album = await Album.findOne({ where: { id: albumId } });
+    if (!album) return res.status(404).json({ error: 'Album not found' });
+
+    const track = await Track.findOne({ where: { id: trackId } });
+    if (!track) return res.status(404).json({ error: 'Track not found' });
+
+    const isTrackAlreadyAdded = album.tracks.some(existingTrack => existingTrack.id === trackId);
+    if (isTrackAlreadyAdded) return res.status(400).json({ error: 'Track is already on the album' });
+
+    album.tracks.push(track);
+    await album.save();
+
+    return res.status(200).json(album);
+  } catch (error) {
+    console.error('Error adding track to album:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const deleteTracksFromAlbum = async (req: Request<{ albumId: number }, {}, { track_ids: number[] }>, res: Response) => {
+  try {
+    const { albumId } = req.params;
+    const { track_ids } = req.body;
+
+    const album = await Album.findOne({ where: { id: albumId }, relations:  ['tracks'] });
+    if (!album) return res.status(404).json({ error: 'Album not found' });
+    console.log(album);
+
+    for (const trackId of track_ids) {
+      album.tracks = album.tracks.filter(track => track.id !== trackId);
+    }
+    await album.save();
+
+    return res.status(200).json(album);
+  } catch (error) {
+    console.error('Error deleting tracks from album:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const deleteAlbum = async (req: Request<{ albumId: number }, {}, {}>, res: Response) => {
+  try {
+    const { albumId } = req.params;
+    const album = await Album.findOne({ where: { id: albumId } });
+    if (!album) return res.status(404).json({ error: 'Album not found' });
+
+    await album.remove();
+
+    return res.status(200).json({ message: 'Album deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting album:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const albumExistsByName = async (name: string): Promise<boolean> => {
+  const album = await Album.findOne({ where: { name: name }});
+  return !!album;
 };
--- albumcreator-backend-node/src/routes/v1/album.ts
+++ albumcreator-backend-node/src/routes/v1/album.ts
@@ -1,4 +1,4 @@
-import { getAlbum, getAlbums } from '../../controllers/album';
+import { addTrackToAlbum, createAlbum, deleteAlbum, deleteTracksFromAlbum, getAlbum, getAlbums } from '../../controllers/album';
 import express from 'express';
 
 const router = express.Router();
@@ -7,14 +7,18 @@ const router = express.Router();
 router.get('/', getAlbums);
 
 // Get all tracks for album
-router.get('/:albumId', getAlbum)
+router.get('/:albumId/tracks', getAlbum)
 
 // Create new album
+router.post('/', createAlbum)
 
 // Add track to album
+router.put('/:albumId/tracks/:trackId', addTrackToAlbum);
 
 // Delete tracks from album
+router.delete('/:albumId/tracks', deleteTracksFromAlbum);
 
 // Delete album
+router.delete('/:albumId', deleteAlbum);
 
 export { router as albumRouter };
