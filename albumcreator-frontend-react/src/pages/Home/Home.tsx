f14d2adf3ceb69217e1b73a04a4dbca63c7813a4
Owen Youngman <youngman.owen@gmail.com>
2024-04-28 21:27:33 +0100

    feat: add ability to add track to album

--- albumcreator-frontend-react/src/pages/Home/Home.tsx
+++ albumcreator-frontend-react/src/pages/Home/Home.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
+import AlbumList from '../../components/AlbumList';
 import TrackList from '../../components/TrackList';
+import { Album } from '../../types/album';
+import { AlbumContext } from '../../contexts';
 
 const Home: React.FC = () => {
+  const [albums, setAlbums] = useState<Album[]>([]);
   return (
     <>
       <h1>Album Creator</h1>
-      <TrackList />
+      <AlbumContext.Provider value={{ albums, setAlbums }}> 
+        <TrackList />
+        <AlbumList/>
+      </AlbumContext.Provider>
     </>
   );
 };
