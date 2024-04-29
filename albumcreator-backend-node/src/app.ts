73afa8a69edd153126af5d3275dca375af4192bb
Owen Youngman <youngman.owen@gmail.com>
2024-04-29 16:49:21 +0100

    feat: create endpoint for swagger docs

--- albumcreator-backend-node/src/app.ts
+++ albumcreator-backend-node/src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import v1Routes from './routes/v1';
 import cors from 'cors';
+import * as path from 'path';
+import { docsRouter } from './routes/docs';
 
 const app = express();
 app.use(express.json());
 app.use(cors())
 
-// Define routes
+app.use('/api-docs', express.static(path.join(__dirname, 'node_modules', 'swagger-ui-dist')));
+app.use('/api/docs', docsRouter);
 
 app.use('/api/v1', v1Routes);
 
